7bd8e275d0bb9dd8dd78cb1c5c8f53a600cc98f1
Pablo Ridolfi <pridolfi@users.noreply.github.com>
2018-01-30 08:27:40 -0300

    Update program.py

--- src/program.py
+++ src/program.py
@@ -4,16 +4,17 @@ import wiringpi
 import time
 import sdnotify
 
+OUTPUT = 20
+
 n = sdnotify.SystemdNotifier()
 n.notify("READY=1")
 
 wiringpi.wiringPiSetupGpio()
-wiringpi.pinMode(21, 1)
+wiringpi.pinMode(OUTPUT, 1)
 
 while True:
-    wiringpi.digitalWrite(21, 1)
+    wiringpi.digitalWrite(OUTPUT, 1)
     time.sleep(1)
-    wiringpi.digitalWrite(21, 0)
+    wiringpi.digitalWrite(OUTPUT, 0)
     time.sleep(1)
     n.notify("WATCHDOG=1")
-
